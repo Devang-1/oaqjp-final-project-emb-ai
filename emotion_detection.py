335b8942c7fcdd929f3c9cf38621bbc33b595afa
Devang-1 <devangshirode.27@gmail.com>
2025-04-15 08:17:27 -0400

    Added emotion detection application- joy,anger,disgust,fear,sadness

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -9,4 +10,27 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=input_json)
-    return response.text
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+    
+    # Extract scores
+    anger = emotions.get('anger', 0)
+    disgust = emotions.get('disgust', 0)
+    fear = emotions.get('fear', 0)
+    joy = emotions.get('joy', 0)
+    sadness = emotions.get('sadness', 0)
+
+    # Determine dominant emotion
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = dominant_emotion
+
+    return emotion_scores
